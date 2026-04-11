50c2f1bee744827465bba35ec9e31f87fb3e2afb
hoangnhatnam <hoangnhatnam.dev@gmail.com>
2026-04-11 16:28:13 +0700

    small change

--- luutru.c
+++ luutru.c
@@ -7,7 +7,7 @@ void luuFile (FILE *a,int n, giaodich *t) {
     fprintf(a,"\n%-5s %-25s %-10s %-10s %-12s\n", "STT", "Ten khoan", "Loai", "So tien", "Ngay");
     fprintf(a,"-----------------------------------------------------------------\n");
 
-    for (int i = 0; i < n; i++) {
+    for ( i = 0; i < n; i++) {
         fprintf(a,"%-5d %-25s %-10s %-10d %02d/%02d/%04d\n",
         i + 1,
         t[i].tenkhoan,
